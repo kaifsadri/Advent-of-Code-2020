1adf79a36d886291f2cf48d88775654e14d2ad96
Kai F. Sadri <58483176+kaifsadri@users.noreply.github.com>
2020-12-14 13:44:13 -0500

    Update 13_2.py

--- 13_2.py
+++ 13_2.py
@@ -1,7 +1,6 @@
 L = [i.strip() for i in open("input_13.txt").readlines()]
 Busses = [[i[0], int(i[1])] for i in enumerate(L[1].split(",")) if i[1] not in "xX"]
 Busses.sort(key=lambda x: x[1], reverse=True)
-# print(Busses)
 
 # Bus frequencies are all prime numbers, so there is no overlap
 # this is an implementation of a simplified sieving Chinese Remainder Theorem

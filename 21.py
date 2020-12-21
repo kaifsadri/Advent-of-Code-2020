eb3f5a3e6612630aec8775d6b683cb48da5c4d5f
Kai F. Sadri <58483176+kaifsadri@users.noreply.github.com>
2020-12-21 14:21:11 -0500

    Add files via upload

---
+++ 21.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+from functools import reduce
+
+L = [line.strip() for line in open("input_21.txt").readlines()]
+
+S = defaultdict(lambda: list())
+ING = set()
+
+for line in L:
+    ing, alg = line.split(" (contains ")
+    alg = alg[:-1].split(", ")
+    ing = ing.split(" ")
+    ING = ING.union(set(ing))
+    for allergene in alg:
+        S[allergene].append(ing)
+
+for alg in S:
+    S[alg] = list(reduce(set.intersection, [set(i) for i in S[alg]]))
+
+T = dict()
+finished = False
+while S.values():
+    finished = True
+    s = S.copy()
+    for alg in S:
+        if len(S[alg]) == 1:
+            T[S[alg][0]] = alg
+            s.pop(alg)
+            finished = False
+        else:
+            for item in S[alg]:
+                if item in T:
+                    S[alg].remove(item)
+    S = s
+
+ING = ING - set(T.keys())
+C = 0
+for line in L:
+    for item in line.split(" "):
+        if item in ING:
+            C += 1
+
+print(f"Part 1: {C}")
+print(f'Part 2:print(sum([1 for _ in C) {",".join(sorted(T, key=lambda x: T[x]))}')

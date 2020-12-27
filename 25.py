e9cab48f5841c029d3984616ac0ac2f61a14f1b4
Kai F. Sadri <58483176+kaifsadri@users.noreply.github.com>
2020-12-26 20:22:52 -0500

    Update 25.py

--- 25.py
+++ 25.py
@@ -1,12 +1,5 @@
-K = [12320657, 9659666]  # Puzzle input
-M = 20201227
-
-# Transform function is equivalent to python's pow(7, loop, 20201227)
-# but finding by increasing the loop one by one is faster O(n)
-loop = 0
-value = 1
-while value != K[1]:  # Choosing the smaller key, but not sure it will be faster
-    value = (7 * value) % M
-    loop += 1
-
-print(pow(K[0], loop, M))
+value = answer = 1
+while value != 9659666:  # Puzzle input was 12320657, 9659666
+    value = (7 * value) % 20201227
+    answer = (12320657 * answer) % 20201227
+print(answer)
